7006881a92be3c810d462ce320f38690adfa4db7
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-06-03 17:02:32 +0600

    statistics page with rechart

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const QuizData = useLoaderData()
     return (
-        <div>
-            
+        <div className='lg:px-96 md:mt-5 px-0 mt-5'>
+            <div>
+                <ResponsiveContainer width='92%' height={400}>
+                    <BarChart data={QuizData.data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="total" fill="#0ea5e9" />
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
         </div>
     );
 };
