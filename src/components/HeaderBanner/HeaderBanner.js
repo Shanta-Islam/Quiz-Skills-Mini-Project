268180c1fc05054e52ab693b0f736f7d83f05b81
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-06-03 16:50:10 +0600

    header banner

--- src/components/HeaderBanner/HeaderBanner.js
+++ src/components/HeaderBanner/HeaderBanner.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Banner from '../../images/banner.jpg';
 
 const HeaderBanner = () => {
     return (
-        <div>
-            <h3>header Banner</h3>
+        <div className='lg:flex justify-around items-center lg:px-20 px-10 lg:py-0 py-10'>
+            <div className='headerText text-left'>
+                <h2 className='text-3xl font-medium'>Easy and Intuitive <span className='text-sky-500'>Online Quiz</span></h2>
+                <p className='mt-4'>QuizSkills is a fun site that can test your knowledge and make you happier at the same time.This is an incredible resource and is extremely versatile; it is a beneficial platform for people of all ages and abilities.</p>
+            </div>
+            <img src={Banner} alt="" className='lg:w-2/4 w-full'/>
         </div>
     );
 };
